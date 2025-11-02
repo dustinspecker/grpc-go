f2894310ccd438d6067388219e069255310ee793
Dustin Specker <dustin.specker@goteleport.com>
2025-11-02 10:55:48 -0600

    comment what mu guards

--- examples/features/health/client/client/teleport_pick_healthy.go
+++ examples/features/health/client/client/teleport_pick_healthy.go
@@ -83,13 +83,15 @@ func (teleportPickHealthyBuilder) Name() string {
 // aaddress/endpoint is tried again. This provides the opportunity for the load balancer to forward the request to another server.
 type teleportPickHealthyBalancer struct {
 	// cc and opts are used for creating new pick_first_leaf load balancers
+	// cc and opts are never written once configured in [teleportPickHealthyBuilder.Build]
 	cc   balancer.ClientConn
 	opts balancer.BuildOptions
 
 	log *slog.Logger
 
 	// mu lock should be held before accessing resolvedState or current/pending balancers
-	mu            sync.Mutex
+	mu sync.Mutex
+	// resolvedState is used when creating new pick_first_leaf_balancers
 	resolvedState resolver.State
 	current       *wrappedBalancer
 	pending       *wrappedBalancer
@@ -156,9 +158,9 @@ func (t *teleportPickHealthyBalancer) UpdateClientConnState(state balancer.Clien
 
 	t.mu.Lock()
 	t.resolvedState = state.ResolverState
+	t.mu.Unlock()
 
 	state.ResolverState = pickfirstleaf.EnableHealthListener(state.ResolverState)
-	t.mu.Unlock()
 
 	return bal.UpdateClientConnState(state)
 }
